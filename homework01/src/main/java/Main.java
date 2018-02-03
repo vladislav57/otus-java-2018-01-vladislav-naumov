78b8899d4cafece1e1a5d9d945c897732ae3194f
vlad <vlad.naumov57@gmail.com>
2018-02-03 12:32:03 +0300

    Добавлена основная часть (без обфускации) домашнего задания 1.
    Сборка работает по
    mvn package
    
    Запуск выполняется по
    java -jar ./target/homework_01_project_name.jar arg1 arg2
    
    Приложение собирает строки из входящих аргументов с помощью guava и выводит на экран
    Пример для указанной строки запуска:
    You entered: arg1, arg2

---
+++ homework01/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import com.google.common.base.Joiner;
+
+public class Main {
+    public static void main(String[] args) {
+        testGuavaLibrary(args);
+    }
+
+    private static void testGuavaLibrary(String[] args) {
+        if(args.length == 0) {
+            System.out.println("Please, enter some arguments");
+        } else {
+            System.out.println("You entered: " + Joiner.on("; ").join(args));
+        }
+    }
+}
