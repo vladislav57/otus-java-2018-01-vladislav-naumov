db79bfb7376917ab1a40ca21515331c2edb8b033
Naumov Vladislav <vlad.naumov57@gmail.com>
2018-02-13 23:47:47 +0300

    Added code for measuring collections

--- homework02/src/main/java/ru/otus/hw2/Main.java
+++ homework02/src/main/java/ru/otus/hw2/Main.java
@@ -7,38 +7,55 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("pid: " + ManagementFactory.getRuntimeMXBean().getName());
 
-        int max_array_size = 10_000_000;
+        int base_array_size = 100_000_000;
 
+        int max_collection_size = 100_000;
         //нужно определить рост размера контейнера в зависимости от числа элементов
         //аналогично посмотрим на размер отдельного объекта
-        for(int current_size = 1; current_size < max_array_size; current_size *= 10 ) {
-            System.gc();
-            Thread.sleep(100);
-            Runtime runtime = Runtime.getRuntime();
-            long mem = runtime.totalMemory() - runtime.freeMemory();
-            System.out.println(mem);
+        for(int current_size = 1; current_size < max_collection_size; current_size *= 10 ) {
+            long mem = usedMemory();
+            System.out.println("Used memory before " + mem);
 
             Object[] array = new Object[current_size];
 
-            long mem2 = runtime.totalMemory() - runtime.freeMemory();
-            System.out.println(mem2 - mem);
-            System.out.println((mem2 - mem)/current_size);
+            long mem2 = usedMemory();
+            double averageRef = 1.0d * (mem2 - mem)/current_size;
+            System.out.println("Difference in memory " + (mem2 - mem));
+            System.out.println("average memory per object reference " + averageRef);
 
-            for (int i = 0; i < current_size; i++) {
-                array[i] = new Object();
+            for (int i = 0; i < base_array_size; i++) {
+                //array[i] = new Object();
                 //array[i] = new String(""); //String pool
                 //array[i] = new String(new char[0]); //without String pool
-                //array[i] = new MyClass();
+                //array[i] = new ArrayList<>();
+                //array[i] = new LinkedList<>();
+                //array[i] = new HashSet<>();
+                //array[i] = new TreeSet<>();
+                //array[i] = new HashMap<>();
+                //array[i] = new TreeMap<>();
             }
 
-            long mem3 = runtime.totalMemory() - runtime.freeMemory();
-            System.out.println((mem3 - mem2)/current_size);
+            long mem3 = usedMemory();
+            double averageObj = 1.0d * (mem3 - mem2)/current_size;
+            System.out.println("average memory per object " + averageObj);
 
             System.out.println("Created " + current_size + " objects.");
+
+            for(int i = 0; i < current_size; i++) {
+                array[i] = null;
+            }
+            array = null;
 
             Thread.sleep(1000); //wait for 1 sec
         }
 
+    }
+
+    public static long usedMemory() throws InterruptedException {
+        System.gc();
+        Thread.sleep(100);
+        Runtime runtime = Runtime.getRuntime();
+        return runtime.totalMemory() - runtime.freeMemory();
     }
 
 }
